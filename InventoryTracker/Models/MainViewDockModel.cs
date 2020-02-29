146fa1d9d521a7668c937f40b5ab36285906538f
Sachira Chinthana Jayasanka <sachira@LIVE.COM>
2020-02-29 11:24:21 -0500

    Left pane dock added

---
+++ InventoryTracker/Models/MainViewDockModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace InventoryTracker.Models
+{
+    public class MainViewDockModel : ObservableObject
+    {
+        private bool _showRightPane;
+        private int _inventoryGridColumnSpan;
+        private bool _showMiddleButton;
+        private int _leftPaneGridsColumnSpan;
+        private bool _showRightButton;
+
+        public bool ShowRightPane
+        {
+            get => _showRightPane;
+            set
+            {
+                _showRightPane = value;
+                RaisePropertyChanged();
+
+                if (value)
+                {
+                    InventoryGridColumnSpan = 1;
+                    ShowMiddleButton = true;
+                    LeftPaneGridsColumnSpan = 2;
+                    ShowRightButton = false;
+                }
+                else
+                {
+                    InventoryGridColumnSpan = 3;
+                    ShowMiddleButton = false;
+                    LeftPaneGridsColumnSpan = 1;
+                    ShowRightButton = true;
+                }
+            }
+        }
+
+        public int InventoryGridColumnSpan
+        {
+            get => _inventoryGridColumnSpan;
+            set
+            {
+                _inventoryGridColumnSpan = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool ShowMiddleButton
+        {
+            get => _showMiddleButton;
+            set
+            {
+                _showMiddleButton = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int LeftPaneGridsColumnSpan
+        {
+            get => _leftPaneGridsColumnSpan;
+            set
+            {
+                _leftPaneGridsColumnSpan = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool ShowRightButton
+        {
+            get => _showRightButton;
+            set
+            {
+                _showRightButton = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public MainViewDockModel(bool showRightPane)
+        {
+            ShowRightPane = showRightPane;
+        }
+    }
+}
--- InventoryTracker/ViewModels/MainViewModel.cs
+++ InventoryTracker/ViewModels/MainViewModel.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using AutoMapper;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using GalaSoft.MvvmLight.Views;
 using InventoryTracker.DataContext;
 using InventoryTracker.Helpers;
 using InventoryTracker.Models;
@@ -21,10 +16,103 @@ namespace InventoryTracker.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        private string _connectionString;
-        private readonly IWindowNavigationService _navigationService;
+        #region Private fields
+
         private readonly IMapper _mapper;
+        private readonly IWindowNavigationService _navigationService;
+        private string _connectionString;
+
+        private ObservableCollection<DivisionModel> _divisions;
+
+        private bool _enableDivisions = true;
+
+        private bool _enableInventoryItems = true;
+
+        private bool _enableRegions = true;
+
+        private ObservableCollection<InventoryItemModel> _inventoryItems;
+
+        private MainViewDockModel _mainViewDock = new MainViewDockModel(true);
+
         private ObservableCollection<RegionModel> _regions;
+
+        private int? _searchDivisionId;
+
+        private ObservableCollection<DivisionModel> _searchFilteredDivisions;
+
+        private ObservableCollection<RegionModel> _searchFilteredRegions;
+
+        private int? _searchRegionId;
+
+        private string _searchText;
+
+        private InventoryItemModel _selectedInventoryItem;
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<DivisionModel> Divisions
+        {
+            get => _divisions;
+            set
+            {
+                _divisions = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool EnableDivisions
+        {
+            get => _enableDivisions;
+            set
+            {
+                _enableDivisions = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool EnableInventoryItems
+        {
+            get => _enableInventoryItems;
+            set
+            {
+                _enableInventoryItems = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool EnableRegions
+        {
+            get => _enableRegions;
+            set
+            {
+                _enableRegions = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand HideLeftPaneCommand { get; set; }
+
+        public ObservableCollection<InventoryItemModel> InventoryItems
+        {
+            get => _inventoryItems;
+            set
+            {
+                _inventoryItems = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public MainViewDockModel MainViewDock
+        {
+            get => _mainViewDock;
+            set
+            {
+                _mainViewDock = value;
+                RaisePropertyChanged();
+            }
+        }
 
         public ObservableCollection<RegionModel> Regions
         {
@@ -40,79 +128,33 @@ namespace InventoryTracker.ViewModels
             }
         }
 
-        private bool _enableRegions = true;
+        public RelayCommand SaveDivisionsCommand { get; set; }
 
-        public bool EnableRegions
+        public RelayCommand SaveInventoryItemsCommand { get; set; }
+        
+        public RelayCommand SaveRegionsCommand { get; set; }
+        
+        public RelayCommand SearchCommand { get; set; }
+
+        public int? SearchDivisionId
         {
-            get => _enableRegions;
+            get => _searchDivisionId;
             set
             {
-                _enableRegions = value;
+                _searchDivisionId = value;
                 RaisePropertyChanged();
             }
         }
 
-        private ObservableCollection<DivisionModel> _divisions;
-
-        public ObservableCollection<DivisionModel> Divisions
+        public ObservableCollection<DivisionModel> SearchFilteredDivisions
         {
-            get => _divisions;
+            get => _searchFilteredDivisions;
             set
             {
-                _divisions = value;
+                _searchFilteredDivisions = value;
                 RaisePropertyChanged();
             }
         }
-
-        private bool _enableDivisions = true;
-
-        public bool EnableDivisions
-        {
-            get => _enableDivisions;
-            set
-            {
-                _enableDivisions = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private ObservableCollection<InventoryItemModel> _inventoryItems;
-
-        public ObservableCollection<InventoryItemModel> InventoryItems
-        {
-            get => _inventoryItems;
-            set
-            {
-                _inventoryItems = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private bool _enableInventoryItems = true;
-
-        public bool EnableInventoryItems
-        {
-            get => _enableInventoryItems;
-            set
-            {
-                _enableInventoryItems = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private InventoryItemModel _selectedInventoryItem;
-
-        public InventoryItemModel SelectedInventoryItem
-        {
-            get => _selectedInventoryItem;
-            set
-            {
-                _selectedInventoryItem = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private ObservableCollection<RegionModel> _searchFilteredRegions;
 
         public ObservableCollection<RegionModel> SearchFilteredRegions
         {
@@ -124,7 +166,7 @@ namespace InventoryTracker.ViewModels
             }
         }
 
-        private int? _searchRegionId;
+        public RelayCommand<KeyEventArgs> SearchKeyUpCommand { get; set; }
 
         public int? SearchRegionId
         {
@@ -138,38 +180,9 @@ namespace InventoryTracker.ViewModels
                 list.Insert(0, new DivisionModel());
                 SearchFilteredDivisions = new ObservableCollection<DivisionModel>(list);
 
-                if (SearchFilteredDivisions.Any(e => e.DivisionId == SearchDivisionId) == false)
-                {
-                    SearchDivisionId = null;
-                }
+                if (SearchFilteredDivisions.Any(e => e.DivisionId == SearchDivisionId) == false) SearchDivisionId = null;
             }
         }
-
-        private ObservableCollection<DivisionModel> _searchFilteredDivisions;
-
-        public ObservableCollection<DivisionModel> SearchFilteredDivisions
-        {
-            get => _searchFilteredDivisions;
-            set
-            {
-                _searchFilteredDivisions = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private int? _searchDivisionId;
-
-        public int? SearchDivisionId
-        {
-            get => _searchDivisionId;
-            set
-            {
-                _searchDivisionId = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private string _searchText;
 
         public string SearchText
         {
@@ -181,12 +194,23 @@ namespace InventoryTracker.ViewModels
             }
         }
 
+        public InventoryItemModel SelectedInventoryItem
+        {
+            get => _selectedInventoryItem;
+            set
+            {
+                _selectedInventoryItem = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand ShowLeftPaneCommand { get; set; }
+
         public RelayCommand<RoutedEventArgs> ViewLoadedCommand { get; set; }
-        public RelayCommand SaveRegionsCommand { get; set; }
-        public RelayCommand SaveDivisionsCommand { get; set; }
-        public RelayCommand SaveInventoryItemsCommand { get; set; }
-        public RelayCommand SearchCommand { get; set; }
-        public RelayCommand<KeyEventArgs> SearchKeyUpCommand { get; set; }
+
+        #endregion
+
+        #region Constructors And Destructors
 
         public MainViewModel(IWindowNavigationService navigationService, IMapper mapper)
         {
@@ -198,25 +222,17 @@ namespace InventoryTracker.ViewModels
             SaveInventoryItemsCommand = new RelayCommand(SaveInventoryItemsCommandHandler);
             SearchCommand = new RelayCommand(SearchCommandHandler);
             SearchKeyUpCommand = new RelayCommand<KeyEventArgs>(SearchKeyUpCommandHandler);
+            HideLeftPaneCommand = new RelayCommand(HideLeftPaneCommandHandler);
+            ShowLeftPaneCommand = new RelayCommand(ShowLeftPaneCommandHandler);
         }
 
-        private async void ViewLoadedCommandHandler(RoutedEventArgs args)
+        #endregion
+
+        #region Private members
+
+        private void HideLeftPaneCommandHandler()
         {
-            _connectionString = (string)_navigationService.ShowDialog("DatabaseConnection");
-            await LoadRegions();
-            await LoadDivisions();
-            await LoadInventoryItems();
-        }
-
-        private async Task LoadRegions()
-        {
-            EnableRegions = false;
-
-            await using var dataContext = new InventoryDataContext(_connectionString);
-            var regions = await dataContext.Regions.ToListAsync();
-            Regions = new ObservableCollection<RegionModel>(_mapper.Map<List<RegionModel>>(regions));
-
-            EnableRegions = true;
+            MainViewDock = new MainViewDockModel(false);
         }
 
         private async Task LoadDivisions()
@@ -227,10 +243,7 @@ namespace InventoryTracker.ViewModels
             var divisions = await dataContext.Divisions.ToListAsync();
             Divisions = new ObservableCollection<DivisionModel>(_mapper.Map<List<DivisionModel>>(divisions));
 
-            foreach (var divisionModel in Divisions)
-            {
-                divisionModel.Region = Regions.FirstOrDefault(e => e.RegionId == divisionModel.RegionId);
-            }
+            foreach (var divisionModel in Divisions) divisionModel.Region = Regions.FirstOrDefault(e => e.RegionId == divisionModel.RegionId);
 
             EnableDivisions = true;
         }
@@ -243,19 +256,19 @@ namespace InventoryTracker.ViewModels
             var inventoryItems = await dataContext
                 .InventoryItems
                 .AsNoTracking()
-                .Where(i => 
+                .Where(i =>
                     (regionId == null || regionId <= 0 || i.RegionId == regionId)
                     && (divisionId == null || divisionId <= 0 || i.DivisionId == divisionId)
                     && (searchText == null || searchText == ""
                                            || i.InventoryItemId.ToString().Contains(searchText)
-                                           || i.BorrowerName.Contains(searchText) 
-                                           || i.BorrowerNIC.Contains(searchText) 
-                                           || i.ItemSerialNumber.Contains(searchText) 
-                                           || i.ItemType.Contains(searchText) 
+                                           || i.BorrowerName.Contains(searchText)
+                                           || i.BorrowerNIC.Contains(searchText)
+                                           || i.ItemSerialNumber.Contains(searchText)
+                                           || i.ItemType.Contains(searchText)
                                            || i.Region.RegionName.Contains(searchText)
                                            || i.Division.DivisionName.Contains(searchText)
                                            || i.Unit.Contains(searchText)
-                                           )
+                    )
                 )
                 .ToListAsync();
             InventoryItems = new ObservableCollection<InventoryItemModel>(_mapper.Map<List<InventoryItemModel>>(inventoryItems));
@@ -271,31 +284,13 @@ namespace InventoryTracker.ViewModels
             EnableInventoryItems = true;
         }
 
-        private async void SaveRegionsCommandHandler()
+        private async Task LoadRegions()
         {
             EnableRegions = false;
 
-            await using (var dataContext = new InventoryDataContext(_connectionString))
-            {
-
-                foreach (var regionModel in Regions)
-                {
-                    var region = _mapper.Map<Region>(regionModel);
-                    if (region.RegionId > 0)
-                    {
-                        dataContext.Entry(region).State = EntityState.Modified;
-                    }
-                    else
-                    {
-                        dataContext.Regions.Add(region);
-                    }
-
-                    await dataContext.SaveChangesAsync();
-                }
-            }
-
-            await LoadRegions();
-
+            await using var dataContext = new InventoryDataContext(_connectionString);
+            var regions = await dataContext.Regions.ToListAsync();
+            Regions = new ObservableCollection<RegionModel>(_mapper.Map<List<RegionModel>>(regions));
             EnableRegions = true;
         }
 
@@ -305,7 +300,6 @@ namespace InventoryTracker.ViewModels
 
             await using (var dataContext = new InventoryDataContext(_connectionString))
             {
-
                 foreach (var divisionModel in Divisions)
                 {
                     var division = _mapper.Map<Division>(divisionModel);
@@ -315,13 +309,9 @@ namespace InventoryTracker.ViewModels
                     division.RegionId = division.Region.RegionId;
                     division.Region = null;
                     if (division.DivisionId > 0)
-                    {
                         dataContext.Entry(division).State = EntityState.Modified;
-                    }
                     else
-                    {
                         dataContext.Divisions.Add(division);
-                    }
 
                     await dataContext.SaveChangesAsync();
                 }
@@ -338,7 +328,6 @@ namespace InventoryTracker.ViewModels
 
             await using (var dataContext = new InventoryDataContext(_connectionString))
             {
-
                 foreach (var inventoryItemModel in InventoryItems)
                 {
                     if (inventoryItemModel.Region == null || inventoryItemModel.Region.RegionId <= 0 || inventoryItemModel.Division == null || inventoryItemModel.Division.DivisionId <= 0)
@@ -350,20 +339,16 @@ namespace InventoryTracker.ViewModels
                     var newHash = inventoryItemModel.GetHash();
                     if (newHash.Equals(inventoryItemModel.ItemHash))
                         continue;
-                    
+
                     var inventoryItem = _mapper.Map<InventoryItem>(inventoryItemModel);
 
                     inventoryItem.Region = null;
                     inventoryItem.Division = null;
 
                     if (inventoryItem.InventoryItemId > 0)
-                    {
                         dataContext.Entry(inventoryItem).State = EntityState.Modified;
-                    }
                     else
-                    {
                         dataContext.InventoryItems.Add(inventoryItem);
-                    }
 
                     await dataContext.SaveChangesAsync();
                 }
@@ -372,6 +357,29 @@ namespace InventoryTracker.ViewModels
             await LoadInventoryItems();
 
             EnableInventoryItems = true;
+        }
+
+        private async void SaveRegionsCommandHandler()
+        {
+            EnableRegions = false;
+
+            await using (var dataContext = new InventoryDataContext(_connectionString))
+            {
+                foreach (var regionModel in Regions)
+                {
+                    var region = _mapper.Map<Region>(regionModel);
+                    if (region.RegionId > 0)
+                        dataContext.Entry(region).State = EntityState.Modified;
+                    else
+                        dataContext.Regions.Add(region);
+
+                    await dataContext.SaveChangesAsync();
+                }
+            }
+
+            await LoadRegions();
+
+            EnableRegions = true;
         }
 
         private async void SearchCommandHandler()
@@ -386,5 +394,20 @@ namespace InventoryTracker.ViewModels
 
             SearchCommandHandler();
         }
+
+        private void ShowLeftPaneCommandHandler()
+        {
+            MainViewDock = new MainViewDockModel(true);
+        }
+
+        private async void ViewLoadedCommandHandler(RoutedEventArgs args)
+        {
+            _connectionString = (string) _navigationService.ShowDialog("DatabaseConnection");
+            await LoadRegions();
+            await LoadDivisions();
+            await LoadInventoryItems();
+        }
+
+        #endregion
     }
 }
